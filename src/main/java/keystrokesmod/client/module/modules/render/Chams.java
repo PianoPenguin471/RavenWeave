2f70bef91703bce6c52b4d45a21fcf404e4a41cd
PianoPenguin471 <danielmchristiansen@outlook.com>
2023-04-21 15:38:42 -0700

    Made chams toggleable

--- src/main/java/keystrokesmod/client/module/modules/render/Chams.java
+++ src/main/java/keystrokesmod/client/module/modules/render/Chams.java
@@ -3,12 +3,17 @@ package keystrokesmod.client.module.modules.render;
 import club.maxstats.weave.loader.api.event.RenderLivingEvent;
 import club.maxstats.weave.loader.api.event.SubscribeEvent;
 import keystrokesmod.client.module.Module;
+import keystrokesmod.client.module.modules.world.AntiBot;
+import keystrokesmod.client.module.setting.impl.DescriptionSetting;
+import keystrokesmod.client.module.setting.impl.TickSetting;
 import org.lwjgl.opengl.GL11;
 
 public class Chams extends Module {
     public Chams() {
         super("Chams", ModuleCategory.render);
     }
+
+    public static TickSetting ignoreBots;
 
     @SubscribeEvent
     public void onPreLivingRender(RenderLivingEvent.Pre event) {
@@ -17,12 +22,14 @@ public class Chams extends Module {
             GL11.glEnable(32823);
             GL11.glPolygonOffset(1.0F, -1100000.0F);
         }
+        this.registerSetting(new DescriptionSetting("Shows player through walls"));
+        this.registerSetting(ignoreBots = new TickSetting("Hide bots", true));
     }
 
     @SubscribeEvent
     public void onPostLivingRender(RenderLivingEvent.Post event) {
         if (!this.enabled) return;
-        if (event.getEntity() != mc.thePlayer) {
+        if (event.getEntity() != mc.thePlayer && (!ignoreBots.isToggled() || !AntiBot.bot(event.getEntity()))) {
             GL11.glDisable(32823);
             GL11.glPolygonOffset(1.0F, 1100000.0F);
         }
