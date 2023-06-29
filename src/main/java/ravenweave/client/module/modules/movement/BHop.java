bd40dbb4e9abed83cb1f8ed96200a15005123cd5
zircta <hyeoski+zircta@gmail.com>
2023-06-29 02:13:06 -0400

    BHOP

---
+++ src/main/java/ravenweave/client/module/modules/movement/BHop.java
@@ -0,0 +1,44 @@
+package ravenweave.client.module.modules.movement;
+
+import com.google.common.eventbus.Subscribe;
+import net.weavemc.loader.api.event.RenderGameOverlayEvent;
+import net.weavemc.loader.api.event.SubscribeEvent;
+import ravenweave.client.event.impl.MoveInputEvent;
+import ravenweave.client.main.Raven;
+import ravenweave.client.module.Module;
+import ravenweave.client.module.setting.impl.SliderSetting;
+import ravenweave.client.module.setting.impl.TickSetting;
+import ravenweave.client.utils.Utils;
+import net.minecraft.client.settings.KeyBinding;
+
+public class BHop extends Module {
+    public static SliderSetting pspd;
+    public static TickSetting fastFall;
+
+    public BHop() {
+        super("Bhop", ModuleCategory.movement);
+        this.registerSetting(pspd = new SliderSetting("Speed", 2.0D, 1.0D, 10.0D, 0.2D));
+        this.registerSetting(fastFall = new TickSetting("Fast Fall", false));
+    }
+
+    @Subscribe
+    public void onMoveInput(MoveInputEvent e) {
+        Module fly = Raven.moduleManager.getModuleByName("Fly");
+        if (fly != null && !fly.isEnabled() && Utils.Player.isMoving() && !mc.thePlayer.isInWater()) {
+            if (mc.thePlayer.onGround) {
+                mc.thePlayer.jump();
+            }
+
+            if (fastFall.isToggled()) {
+                if (mc.thePlayer.fallDistance < 2 && mc.thePlayer.fallDistance > 0) {
+                    mc.thePlayer.motionY *= 1.5;
+                }
+            }
+
+            mc.thePlayer.setSprinting(true);
+            double spd = 0.01D * pspd.getInput();
+            double m = (float)(Math.sqrt(mc.thePlayer.motionX * mc.thePlayer.motionX + mc.thePlayer.motionZ * mc.thePlayer.motionZ) + spd);
+            Utils.Player.bop(m);
+        }
+    }
+}
