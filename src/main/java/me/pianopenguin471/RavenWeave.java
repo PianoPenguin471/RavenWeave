eb42cfc2d078853dc4a7f59cb2387fc2e78f43d4
zircta <hyeoski+zircta@gmail.com>
2023-08-03 14:27:24 -0400

    KEEPSPRINT!!!!

--- src/main/java/me/pianopenguin471/RavenWeave.java
+++ src/main/java/me/pianopenguin471/RavenWeave.java
@@ -3,31 +3,31 @@ package me.pianopenguin471;
 import me.pianopenguin471.command.GUICommand;
 import net.weavemc.loader.api.ModInitializer;
 import net.weavemc.loader.api.command.CommandBus;
-import net.weavemc.loader.api.event.EventBus;
-import net.weavemc.loader.api.event.KeyboardEvent;
-import net.weavemc.loader.api.event.ShutdownEvent;
-import net.weavemc.loader.api.event.StartGameEvent;
+import net.weavemc.loader.api.event.*;
 import ravenweave.client.main.Raven;
 import ravenweave.client.module.Module;
 
 public class RavenWeave implements ModInitializer {
     @Override
     public void preInit() {
-        System.out.println("Initializing RavenWeave!");
-
         CommandBus.register(new GUICommand());
-        EventBus.subscribe(KeyboardEvent.class, (keyboardEvent) -> {
-            for (Module module: Raven.moduleManager.getModules()) {
-                module.keybind();
-            }
-        });
+        EventBus.subscribe(this);
+    }
 
-        EventBus.subscribe(ShutdownEvent.class, (shutdownEvent) -> {
-            Raven.moduleManager.getModuleByName("Blink").disable();
-        });
+    @SubscribeEvent
+    public void onKeyPress(KeyboardEvent e) {
+        for (Module module: Raven.moduleManager.getModules()) {
+            module.keybind();
+        }
+    }
 
-        EventBus.subscribe(StartGameEvent.Post.class, startGameEvent -> {
-            Raven.init();
-        });
+    @SubscribeEvent
+    public void onGameStart(StartGameEvent.Post e) {
+        Raven.init();
+    }
+
+    @SubscribeEvent
+    public void onShutdown(ShutdownEvent e) {
+        Raven.moduleManager.getModuleByName("Blink").disable();
     }
 }
---
+++ src/main/java/me/pianopenguin471/mixins/EntityPlayerMixin.java
@@ -0,0 +1,183 @@
+package me.pianopenguin471.mixins;
+
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.entity.*;
+import net.minecraft.entity.boss.EntityDragonPart;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.item.ItemStack;
+import net.minecraft.network.play.server.S12PacketEntityVelocity;
+import net.minecraft.potion.Potion;
+import net.minecraft.stats.AchievementList;
+import net.minecraft.stats.StatBase;
+import net.minecraft.stats.StatList;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.MathHelper;
+import net.minecraft.world.World;
+import net.weavemc.loader.api.event.EventBus;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+import ravenweave.client.event.impl.HitSlowDownEvent;
+import ravenweave.client.main.Raven;
+import ravenweave.client.module.modules.movement.KeepSprint;
+
+@Mixin(priority = 995, value = EntityPlayer.class)
+public abstract class EntityPlayerMixin extends EntityLivingBase {
+
+    public EntityPlayerMixin(World p_i1594_1_) {
+        super(p_i1594_1_);
+    }
+
+    @Shadow
+    public abstract ItemStack getHeldItem();
+
+    @Shadow
+    public abstract void onCriticalHit(Entity p_onCriticalHit_1_);
+
+    @Shadow
+    public abstract void onEnchantmentCritical(Entity p_onEnchantmentCritical_1_);
+
+    @Shadow
+    public abstract void triggerAchievement(StatBase p_triggerAchievement_1_);
+
+    @Shadow
+    public abstract ItemStack getCurrentEquippedItem();
+
+    @Shadow
+    public abstract void destroyCurrentEquippedItem();
+
+    @Shadow
+    public abstract void addStat(StatBase p_addStat_1_, int p_addStat_2_);
+
+    @Shadow
+    public abstract void addExhaustion(float p_addExhaustion_1_);
+
+    /**
+     * @author mc code
+     * @reason keepsprint
+     */
+    @Overwrite
+    public void attackTargetEntityWithCurrentItem(Entity targetEntity) {
+        if (targetEntity.canAttackWithItem()) {
+            if (!targetEntity.hitByEntity(this)) {
+                float f = (float)this.getEntityAttribute(SharedMonsterAttributes.attackDamage).getAttributeValue();
+                int i = 0;
+                float f1 = 0.0F;
+
+                if (targetEntity instanceof EntityLivingBase) {
+                    f1 = EnchantmentHelper.getModifierForCreature(this.getHeldItem(), ((EntityLivingBase)targetEntity).getCreatureAttribute());
+                } else {
+                    f1 = EnchantmentHelper.getModifierForCreature(this.getHeldItem(), EnumCreatureAttribute.UNDEFINED);
+                }
+
+                i = i + EnchantmentHelper.getKnockbackModifier(this);
+
+                if (this.isSprinting()) {
+                    ++i;
+                }
+
+                if (f > 0.0F || f1 > 0.0F) {
+                    boolean flag = this.fallDistance > 0.0F && !this.onGround && !this.isOnLadder() && !this.isInWater() && !this.isPotionActive(Potion.blindness) && this.ridingEntity == null && targetEntity instanceof EntityLivingBase;
+
+                    if (flag && f > 0.0F) {
+                        f *= 1.5F;
+                    }
+
+                    f = f + f1;
+                    boolean flag1 = false;
+                    int j = EnchantmentHelper.getFireAspectModifier(this);
+
+                    if (targetEntity instanceof EntityLivingBase && j > 0 && !targetEntity.isBurning()) {
+                        flag1 = true;
+                        targetEntity.setFire(1);
+                    }
+
+                    double d0 = targetEntity.motionX;
+                    double d1 = targetEntity.motionY;
+                    double d2 = targetEntity.motionZ;
+                    boolean flag2 = targetEntity.attackEntityFrom(DamageSource.causePlayerDamage((EntityPlayer) (Object) this), f);
+
+                    if (flag2) {
+                        if (i > 0) {
+                            targetEntity.addVelocity(-MathHelper.sin(this.rotationYaw * (float)Math.PI / 180.0F) * (float)i * 0.5F, 0.1D, MathHelper.cos(this.rotationYaw * (float)Math.PI / 180.0F) * (float)i * 0.5F);
+
+                            HitSlowDownEvent slowDown = new HitSlowDownEvent(0.6, false);
+                            EventBus.callEvent(slowDown);
+                            this.motionX *= slowDown.getSlowDown();
+                            this.motionZ *= slowDown.getSlowDown();
+                            this.setSprinting(slowDown.isSprinting());
+                        }
+
+                        if (targetEntity instanceof EntityPlayerMP && targetEntity.velocityChanged) {
+                            ((EntityPlayerMP)targetEntity).playerNetServerHandler.sendPacket(new S12PacketEntityVelocity(targetEntity));
+                            targetEntity.velocityChanged = false;
+                            targetEntity.motionX = d0;
+                            targetEntity.motionY = d1;
+                            targetEntity.motionZ = d2;
+                        }
+
+                        if (flag) {
+                            this.onCriticalHit(targetEntity);
+                        }
+
+                        if (f1 > 0.0F) {
+                            this.onEnchantmentCritical(targetEntity);
+                        }
+
+                        if (f >= 18.0F) {
+                            this.triggerAchievement(AchievementList.overkill);
+                        }
+
+                        this.setLastAttacker(targetEntity);
+
+                        if (targetEntity instanceof EntityLivingBase)
+                        {
+                            EnchantmentHelper.applyThornEnchantments((EntityLivingBase)targetEntity, this);
+                        }
+
+                        EnchantmentHelper.applyArthropodEnchantments(this, targetEntity);
+                        ItemStack itemstack = this.getCurrentEquippedItem();
+                        Entity entity = targetEntity;
+
+                        if (targetEntity instanceof EntityDragonPart)
+                        {
+                            IEntityMultiPart ientitymultipart = ((EntityDragonPart)targetEntity).entityDragonObj;
+
+                            if (ientitymultipart instanceof EntityLivingBase)
+                            {
+                                entity = (EntityLivingBase)ientitymultipart;
+                            }
+                        }
+
+                        if (itemstack != null && entity instanceof EntityLivingBase)
+                        {
+                            itemstack.hitEntity((EntityLivingBase)entity, (EntityPlayer) (Object)this);
+
+                            if (itemstack.stackSize <= 0)
+                            {
+                                this.destroyCurrentEquippedItem();
+                            }
+                        }
+
+                        if (targetEntity instanceof EntityLivingBase)
+                        {
+                            this.addStat(StatList.damageDealtStat, Math.round(f * 10.0F));
+
+                            if (j > 0)
+                            {
+                                targetEntity.setFire(j * 4);
+                            }
+                        }
+
+                        this.addExhaustion(0.3F);
+                    }
+                    else if (flag1)
+                    {
+                        targetEntity.extinguish();
+                    }
+                }
+            }
+        }
+    }
+}
