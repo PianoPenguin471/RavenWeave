5082ebd4802d6b0ff4efe84151b49742ff466c5f
Syz <126593996+Syz66@users.noreply.github.com>
2023-10-23 11:52:17 -0400

    Update SlowdownEvent.java

--- src/main/java/ravenweave/client/event/impl/SlowdownEvent.java
+++ src/main/java/ravenweave/client/event/impl/SlowdownEvent.java
@@ -3,8 +3,8 @@ package ravenweave.client.event.impl;
 import net.weavemc.loader.api.event.Event;
 
 public class SlowdownEvent extends Event {
-    float strafeSpeedMultiplier;
-    float forwardSpeedMultiplier;
+    float strafeSpeedMultiplier = 2.0F;
+    float forwardSpeedMultiplier = 2.0F;
 
     public float getStrafeSpeedMultiplier() {
         return strafeSpeedMultiplier;
