d993a8924a4f6824d0df65f439f000ed2597d8fa
PianoPenguin471 <danielmchristiansen@outlook.com>
2023-11-21 13:25:02 -0800

    more useless refactor/fixes

--- src/main/java/ravenweave/client/module/modules/render/ChestESP.java
+++ src/main/java/ravenweave/client/module/modules/render/ChestESP.java
@@ -7,7 +7,6 @@ import net.weavemc.loader.api.event.RenderWorldEvent;
 import net.weavemc.loader.api.event.SubscribeEvent;
 import ravenweave.client.module.Module;
 import ravenweave.client.module.setting.impl.RGBSetting;
-import ravenweave.client.module.setting.impl.SliderSetting;
 import ravenweave.client.module.setting.impl.TickSetting;
 import ravenweave.client.utils.Utils;
 
@@ -20,7 +19,7 @@ public class ChestESP extends Module {
 
     public ChestESP() {
         super("ChestESP", ModuleCategory.render);
-        this.registerSetting(rgb = new RGBSetting("Red", 255, 255, 255));
+        this.registerSetting(rgb = new RGBSetting("Color", 255, 255, 255));
         this.registerSetting(rainbow = new TickSetting("Rainbow", false));
     }
 
@@ -30,19 +29,11 @@ public class ChestESP extends Module {
         if (Utils.Player.isPlayerInGame()) {
             int color = rainbow.isToggled() ? Utils.Client.rainbowDraw(2L, 0L)
                     : (new Color(rgb.getRed(), rgb.getGreen(), rgb.getBlue())).getRGB();
-            Iterator var3 = mc.theWorld.loadedTileEntityList.iterator();
 
-            while (true) {
-                TileEntity te;
-                do {
-                    if (!var3.hasNext()) {
-                        return;
-                    }
-
-                    te = (TileEntity) var3.next();
-                } while (!(te instanceof TileEntityChest) && !(te instanceof TileEntityEnderChest));
-
-                Utils.HUD.re(te.getPos(), color, true);
+            for (TileEntity tileEntity: mc.theWorld.loadedTileEntityList) {
+                if (tileEntity instanceof TileEntityChest || tileEntity instanceof TileEntityEnderChest) {
+                    Utils.HUD.renderBlock(tileEntity.getPos(), color, true);
+                }
             }
         }
     }
