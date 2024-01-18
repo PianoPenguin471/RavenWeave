b40edebee0588478684377a2576223830ea65e45
PianoPenguin471 <danielmchristiansen@outlook.com>
2024-01-18 13:03:29 -0800

    Small change I forgot to commit earlier

--- src/main/java/ravenweave/client/module/modules/beta/Scaffold.java
+++ src/main/java/ravenweave/client/module/modules/beta/Scaffold.java
@@ -12,18 +12,26 @@ import net.minecraft.util.MovingObjectPosition;
 import net.minecraft.util.Vec3;
 import net.weavemc.loader.api.event.RenderGameOverlayEvent;
 import net.weavemc.loader.api.event.SubscribeEvent;
+import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.lwjgl.input.Keyboard;
 import ravenweave.client.event.LookEvent;
 import ravenweave.client.event.UpdateEvent;
 import ravenweave.client.module.Module;
+import ravenweave.client.module.modules.player.BedAura;
 import ravenweave.client.module.setting.impl.DescriptionSetting;
 import ravenweave.client.module.setting.impl.SliderSetting;
 import ravenweave.client.module.setting.impl.TickSetting;
+import ravenweave.client.utils.CaterpillowUtils;
 import ravenweave.client.utils.Utils;
+
+import java.security.KeyPair;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Scaffold extends Module {
 
-    private final TickSetting disableSprint, noSwing, slotSwap, sameY;
+    private final TickSetting disableSprint, noSwing, slotSwap, sameY, doRots;
     private final SliderSetting pitch;
 
     private float yaw, prevYaw;
@@ -34,9 +42,10 @@ public class Scaffold extends Module {
         this.registerSetting(new DescriptionSetting("Helps you make bridges/scaffold walk.")); // bad description, but manthe wrote it
         this.registerSetting(pitch = new SliderSetting("Pitch", 81, 70, 90, 1));
         this.registerSetting(noSwing = new TickSetting("No Swing", false));
-        this.registerSetting(sameY = new TickSetting("Same Y", true));
+        this.registerSetting(sameY = new TickSetting("Same Y", false));
         this.registerSetting(disableSprint = new TickSetting("Disable sprint", true));
         this.registerSetting(slotSwap = new TickSetting("Swap to blocks", true));
+        this.registerSetting(doRots = new TickSetting("Do Rotations", true));
     }
 
     public void onDisable() {
@@ -73,6 +82,28 @@ public class Scaffold extends Module {
         e.setPrevPitch(mc.gameSettings.keyBindJump.isKeyDown() && !mc.gameSettings.keyBindForward.isKeyDown() ? 90 : (float) pitch.getInput());
     }
 
+    public MovingObjectPosition getMouseOverWithoutRots() {
+        BlockPos blockUnder = new BlockPos(mc.thePlayer).down();
+        List<ImmutablePair<BlockPos, EnumFacing>> blockstoPlaceOn = new ArrayList<>();
+        for (int x = -1; x < 1; x++) {
+            for (int y = -1; y < 1; y++) {
+                for (int z = -1; z < 1; z++) {
+                    BlockPos pos = blockUnder.add(x,y,z);
+                    if (CaterpillowUtils.isAirBlock(pos)) continue; // Yes I'm using caterpillow's code, fight me it's a raven skid
+                    blockstoPlaceOn.add(new ImmutablePair<>(pos, EnumFacing.getFacingFromVector(x, y, z)));
+                }
+            }
+        }
+        blockstoPlaceOn.sort((o1, o2) -> (int) (mc.thePlayer.getDistanceSq(o2.left) - mc.thePlayer.getDistanceSq(o1.left)));
+
+        if (blockstoPlaceOn.isEmpty()) return null;
+        BlockPos blockPos = blockstoPlaceOn.get(0).left;
+
+        EnumFacing enumFacing = blockstoPlaceOn.get(0).right;
+        EnumFacing side2 = enumFacing.getOpposite();
+        Vec3 hitVec = new Vec3(blockPos).addVector(0.5, 0.5, 0.5).add(new Vec3(side2.getDirectionVec()));
+        return new MovingObjectPosition(MovingObjectPosition.MovingObjectType.BLOCK, hitVec, enumFacing, blockPos);
+    }
 
     @SubscribeEvent
     public void onRender(RenderGameOverlayEvent event) {
@@ -81,8 +112,8 @@ public class Scaffold extends Module {
 
         if (mc.currentScreen != null || mc.thePlayer.getHeldItem() == null) return;
 
-        MovingObjectPosition mop = mc.objectMouseOver;
-
+        MovingObjectPosition mop = doRots.isToggled() ? mc.objectMouseOver : getMouseOverWithoutRots();
+        if (mop == null) return;
 
         if (shouldClickBlock(mop)) {
             clickBlock(mop.getBlockPos(), mop.sideHit, mop.hitVec);
